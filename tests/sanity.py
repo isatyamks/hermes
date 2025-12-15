50daf9b81b5ec9150152742afa206c40c8342aa7
isatyamks <isatyamks@gmail.com>
2025-12-15 22:53:50 +0530

    Add sanity test for torso height measurement in humanoid environment

---
+++ tests/sanity.py
@@ -0,0 +1,7 @@
+from env.humanoid_env import make_env
+from _logging.metrics import get_torso_height_from_env
+
+env = make_env()
+obs, _ = env.reset()
+
+print("Initial torso height:", get_torso_height_from_env(env))
